56b39c6f2db507884d3da083a31340a29136b700
Zachary <zachjoliver@gmail.com>
2020-01-04 13:03:15 -0800

    Working with helper function for message still need to get timing

--- pokerRandomizer.js
+++ pokerRandomizer.js
@@ -1,7 +1,17 @@
+function message(){
+  console.clear();
+  console.log('Random number below. #\'s indicate aggression %:')
+  let random = Math.random().toFixed(2) * 100;
+  console.log(`${Math.floor(random)}%`);
+}
+
 function randomTimer(){
-  while (true) {
-    console.log(Math.random());
-  }
+  // while (true) {
+  //   setTimeout(message,3000);
+  // }
+  message()
 }
 
 randomTimer();
+
+//setTimeout(function(){ alert("Hello"); }, 3000);
