2f015d45beb0ce68148e9aad0d07bfc32ea25723
Zachary <zachjoliver@gmail.com>
2020-01-04 13:44:33 -0800

    Initial version made with colors and a timer of 3 seconds

--- pokerRandomizer.js
+++ pokerRandomizer.js
@@ -1,17 +1,20 @@
+var colors = require('colors');
+
 function message(){
   console.clear();
-  console.log('Random number below. #\'s indicate aggression %:')
-  let random = Math.random().toFixed(2) * 100;
-  console.log(`${Math.floor(random)}%`);
+  console.log('Higher random numbers below indicate aggression %:')
+  const random = Math.floor(Math.random().toFixed(2) * 100);
+  for (let i = 0; i < 3; i++) { console.log(''); };
+  console.log(`                      ${random.toString()}%`.green);
+  for (let i=0; i < 3; i++) {console.log(''); };
 }
 
 function randomTimer(){
-  // while (true) {
-  //   setTimeout(message,3000);
-  // }
-  message()
+  console.clear();
+  console.log('Welcome to the poker randomizer!'.red);
+  console.log('Random numbers starting in 3 seconds!'.blue);
+  console.log('Good luck player!'.green);
+  setInterval(message, 3000);
 }
 
 randomTimer();
-
-//setTimeout(function(){ alert("Hello"); }, 3000);
