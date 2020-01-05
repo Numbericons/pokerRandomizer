612979fd5854b8b47e58b87397bb1028ff9257b2
Zachary <zachjoliver@gmail.com>
2020-01-05 11:12:15 -0800

    Text color and other minor tweaks

--- pokerRandomizer.js
+++ pokerRandomizer.js
@@ -2,7 +2,7 @@ var colors = require('colors');
 
 function message(){
   console.clear();
-  console.log('Higher random numbers below indicate aggression %:')
+  console.log('Higher random numbers below indicate more aggressive action:'.blue)
   const random = Math.floor(Math.random().toFixed(2) * 100);
   for (let i = 0; i < 3; i++) { console.log(''); };
   console.log(`                      ${random.toString()}%`.green);
@@ -12,9 +12,9 @@ function message(){
 function randomTimer(){
   console.clear();
   console.log('Welcome to the poker randomizer!'.red);
-  console.log('Random numbers starting in 3 seconds!'.blue);
+  console.log('Random numbers starting in 5 seconds!'.blue);
   console.log('Good luck player!'.green);
-  setInterval(message, 3000);
+  setInterval(message, 5000);
 }
 
 randomTimer();
